1241f7f27003e4e06a0354237acbd3f795cee91a
danieleon08 <dflp2005@gmail.com>
2023-07-07 20:20:41 -0500

    inicio de la seccion de operaciones

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # App
 Este es el inicio de la App
 Y vamos a empezar con el menú.
+Tambien va a tener una seccio de operaciones.
---
+++ operaciones.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main() {
+
+    int a=2;
+    int b=3;
+    int resultado=a+b;
+    cout<<"La suma de "<<a<< " y "<<b << " es: "<<resultado<<endl;
+
+    return 0;
+
+}
